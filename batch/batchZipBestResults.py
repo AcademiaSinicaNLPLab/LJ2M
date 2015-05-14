d90f90af7d135a49a0457e30fbb495e4959d3b19
doug919 <doug919@gmail.com>
2015-05-14 12:12:01 +0800

    file naming

--- batch/batchZipBestResults.py
+++ batch/batchZipBestResults.py
@@ -6,4 +6,7 @@ best_res_files = ['aaa.pkl', 'bbb.pkl', 'ccc.pkl', 'ddd.pkl']
 res = output.PredictionResult(best_res_files)
 
 output_filename = 'zipped_best_results.pkl'
-res.dump_summary(output_filename)
+res.dump_results(output_filename)
+
+summary_filename = 'best_result_summary.csv'
+res.dump_
