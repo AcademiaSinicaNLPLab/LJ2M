973cb748d889ee8b3f70390b688c95880bbe7bc1
bs980201 <bs980201@gmail.com>
2015-05-18 13:59:46 +0800

    tfitdf

--- batch/batchBuild_tfidf_data.py
+++ batch/batchBuild_tfidf_data.py
@@ -2,16 +2,16 @@
 import sys, os
 sys.path.append("../")
 
-from features.tfidf import TFIDF
+from features.tfidftest import TFIDF
 
-tfidf_obj = TFIDF('LJ2M','tf3','idf1','tf2')
-tfidf_obj.set(tf2_k=1.0, tf3_k=1.0, tf3_b=0.5)
-tfidf_obj.calculate('/corpus/LJ2M/data/pkl/lj2m_wordlists')
-tfidf_obj.dump('/corpus/LJ2M/data/features/tfidf','tf2k1_tf3k1_tf3b0.5_idf1')
+# tfidf_obj = TFIDF('/corpus/LJ2M/data/features/tfidf','tf1_tf2k1_tf3k1b08_idf1_idf2_idf3','LJ2M','tf3','tf2','tf1','idf1','idf2','idf3')
+# tfidf_obj.set(tf2_k=1.0, tf3_k=1.0, tf3_b=0.8)
+# tfidf_obj.calculate('/corpus/LJ2M/data/pkl/lj2m_wordlists')
+# tfidf_obj.dump()
 
-# tfidf_obj = TFIDF('LJ40K','tf3','idf1','tf2')
-# tfidf_obj.set(tf2_k=1.0, tf3_k=1.0, tf3_b=0.3)
-# tfidf_obj.calculate('/corpus/LJ40K/data/pkl/lj40k_wordlists')
-# tfidf_obj.dump('/corpus/LJ40K/data/features/tfidf','tf2k1_tf3k1_tf3b0.3')
+tfidf_obj = TFIDF('LJ40K','tf1','tf2','tf3','idf1','idf2','idf3')
+tfidf_obj.set(tf2_k=1.0, tf3_k=1.0, tf3_b=0.3)
+tfidf_obj.calculate('/corpus/LJ40K/data/pkl/lj40k_wordlists')
+tfidf_obj.dump('/corpus/LJ40K/data/features/tfidf','0_800_tf1_tf2_tf3k1b03_idf1_idf2_idf3')
 
 print 'finished!!'
